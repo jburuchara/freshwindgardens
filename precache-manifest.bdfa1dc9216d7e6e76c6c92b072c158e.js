e53ad0b189599cc7e1b38d21ae17b564b1cfc7b9
Norman Buruchara <buruch1@stolaf.edu>
2020-03-30 15:41:53 +0300

    Updates

---
+++ precache-manifest.bdfa1dc9216d7e6e76c6c92b072c158e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10e389f50c70ecea6ac742760e7033e",
+    "url": "/freshwindgardens/index.html"
+  },
+  {
+    "revision": "023b868b3c55fd608b30",
+    "url": "/freshwindgardens/static/css/2.23306f5f.chunk.css"
+  },
+  {
+    "revision": "8f7a765435b4559fbe9b",
+    "url": "/freshwindgardens/static/css/main.234a5111.chunk.css"
+  },
+  {
+    "revision": "023b868b3c55fd608b30",
+    "url": "/freshwindgardens/static/js/2.84898e16.chunk.js"
+  },
+  {
+    "revision": "4fe3295419de00405cacb1c50c6956d6",
+    "url": "/freshwindgardens/static/js/2.84898e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f7a765435b4559fbe9b",
+    "url": "/freshwindgardens/static/js/main.b8b5dfd1.chunk.js"
+  },
+  {
+    "revision": "d2177afb6b08d9b81e10",
+    "url": "/freshwindgardens/static/js/runtime-main.f53451f8.js"
+  }
+]);
